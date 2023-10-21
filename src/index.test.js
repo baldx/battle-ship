7ec37959e48ba29c1a8f6c6889a00583e89ed37a
baldx <mr.ahmad.m2006@gmail.com>
2023-10-21 17:34:20 +0200

    add isSunken tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import {emptyShip, shipHit, hit} from "./index.js";
+import {emptyShip, shipHit, hit, isSunk} from "./index.js";
 
 it('object has been called', () => {
     expect(emptyShip).toEqual({ length: undefined, hit: 0, sunk: false })
@@ -10,4 +10,14 @@ it('ship has sunken', () => {
 
 it('ship has been hit', () => {
     expect(hit(emptyShip)).toEqual({ length: undefined, hit: 1, sunk: false })
+})
+
+it('ship is not sunken', () => {
+    const ship = {length: 3, hit: 2, sunk: false}
+    expect(isSunk(ship)).toBe(false);
+})
+
+it('ship sunk', () => {
+    const ship = {length: 3, hit: 3, sunk: true}
+    expect(isSunk(ship)).toBe(true);
 })
