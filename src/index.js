fcb3b7ad0e8f8500de34eace573ac644d6e28714
baldx <mr.ahmad.m2006@gmail.com>
2023-10-23 19:22:30 +0200

    add receive attack function

--- src/index.js
+++ src/index.js
@@ -63,14 +63,36 @@ function placeShip(board, ship, row, col, direction) {
   return board;
 }
 
-function receiveAttack (board) {
-  /*
-    checks board where the ships are
-    if ship is in that coordinate
-    increase the ships hit counter
-    if the same hit is hit in a previous round
-    return error
-  */
+function receiveAttack (board, row, col, ship) {
+  const { x, y } = new Board();
+  
+  let index = (row * y) + col;
+  if (board[index] === 1) {
+    board[index] = 2;
+  }
+
+  const newShip = new Ship(ship)
+
+  if (row >= 0 && row < x && col < y && col >= 0) {
+    let shipIndex = (row * y) + col - (newShip.length - 1);
+
+    if (newShip.direction === 'H' && shipIndex >= 0 && shipIndex < board.length) {
+      newShip.hit++;
+
+      if (newShip.hit === newShip.length) newShip.sunk = true;
+
+    } else if (newShip.direction === 'V' && shipIndex >= 0 && shipIndex < board.length) {
+      newShip.hit++;
+  
+      if (newShip.hit === newShip.length) newShip.sunk = true;
+    }
+    
+    return { board, newShip };
+  } 
+
+  if (board[index] === 0) board[index] = 3;
+  
+  return {board, null: null}
 }
 
-export {emptyShip, shipHit, hit, isSunk, gameBoard, placeShip};
+export {emptyShip, shipHit, hit, isSunk, gameBoard, placeShip, receiveAttack};
